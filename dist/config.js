9e65f095a0a8d884ba447420ea45fed38558438e
anastohh99-hue <anastohh99@gmail.com>
2026-06-26 02:56:26 -0900

    Update config.js

--- dist/config.js
+++ dist/config.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = require("fs");
 const path_1 = require("path");
 const config = {
-    token: 'MTUyMDAxNDY0NjE5MzY4NDQ4MA.Gyzbaa.e9m8z7ckEWjDu_t3t1mwNLnc0X-IT2P6RuQIEo', // token
+    token: 'MTUyMDAxNDY0NjE5MzY4NDQ4MA.GZOvXB.4nP9346btnzzerOisquPLuJKb8DLc6KO5FCv0g', // token
     clientId: '1520014646193684480', // bot id
     mongoUri: 'mongodb+srv://Galbash-Systeam-2:OFOZozOpEnIg2VaI@cluster0.fggmqfp.mongodb.net/?appName=Cluster0', // mongodb url
     defaultPrefix: '!',
